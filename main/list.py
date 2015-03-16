3825e79ec56ee5d86d589d30939c324820ef3317
celeen <celeenrusk@gmail.com>
2015-03-16 14:18:09 -0500

    ordered tests

--- main/list.py
+++ main/list.py
@@ -1,86 +1,86 @@
 from custom_exceptions import *
 
 class MetaList:
+	pass	
+	# def __init__(self, *starting_values):
+	# 	if not starting_values:
+	# 		self.storage = []
+	# 	else:
+	# 		self.storage = list(starting_values)
+
+	# def __getitem__(self, index):
+	# 	return self.storage[index]
+
+	# def __setitem__(self, index, value):
+	# 	self.storage[index] = value
+
+	# def size(self):
+	# 	return len(self.storage)
 	
-	def __init__(self, *starting_values):
-		if not starting_values:
-			self.storage = []
-		else:
-			self.storage = list(starting_values)
-
-	def __getitem__(self, index):
-		return self.storage[index]
-
-	def __setitem__(self, index, value):
-		self.storage[index] = value
-
-	def size(self):
-		return len(self.storage)
+	# def is_empty(self):
+	# 	return not self.storage
 	
-	def is_empty(self):
-		return not self.storage
-	
-	def at(self, position, put=None):
-		# self.empty_check(position)
-		if put is not None:
-			self.storage[position] = put
+	# def at(self, position, put=None):
+	# 	# self.empty_check(position)
+	# 	if put is not None:
+	# 		self.storage[position] = put
 
-		return self.storage[position]
+	# 	return self.storage[position]
 
-	def insert(self, item, position=0):
-		# self.empty_check(position)
-		self.storage.insert(position, item)
+	# def insert(self, item, position=0):
+	# 	# self.empty_check(position)
+	# 	self.storage.insert(position, item)
 
-	def append(self, x):
-		self.insert(x, len(self.storage))
-		return self
+	# def append(self, x):
+	# 	self.insert(x, len(self.storage))
+	# 	return self
 
-	def extend(self, extension):
-		for item in extension:
-			self.storage.append(item)
+	# def extend(self, extension):
+	# 	for item in extension:
+	# 		self.storage.append(item)
 
-		return self.storage
+	# 	return self.storage
 
-	def remove(self, x):
-		self.storage.remove(x)
+	# def remove(self, x):
+	# 	self.storage.remove(x)
 
-	def pop(self, index=-1):
-		self.storage.remove(self[index])
+	# def pop(self, index=-1):
+	# 	self.storage.remove(self[index])
 
-	def index(self, x):
-		for i in range(self.size()):
-			if self[i] == x:
-				return i
-		else:
-			raise ItemNotFound
+	# def index(self, x):
+	# 	for i in range(self.size()):
+	# 		if self[i] == x:
+	# 			return i
+	# 	else:
+	# 		raise ItemNotFound
 
-	def count(self, x):
-		instances = 0
-		for item in self:
-			if item == x:
-				instances += 1
+	# def count(self, x):
+	# 	instances = 0
+	# 	for item in self:
+	# 		if item == x:
+	# 			instances += 1
 
-		return instances
+	# 	return instances
 
-	#  -----------BONUS---------------
+	# #  -----------BONUS---------------
 
-	def sort(self):
-		for i in range(1, self.size()):
-			j = i
-			while j > 0 and self[j] < self[j - 1]:
-				self[j], self[j - 1] = self[j - 1], self[j]
-				j -= 1
+	# def sort(self):
+	# 	for i in range(1, self.size()):
+	# 		j = i
+	# 		while j > 0 and self[j] < self[j - 1]:
+	# 			self[j], self[j - 1] = self[j - 1], self[j]
+	# 			j -= 1
 		
-		return self
+	# 	return self
 		
-	def reverse(self):
-		j = self.size() - 1
+	# def reverse(self):
+	# 	j = self.size() - 1
 	
-		for i in range(self.size()):
-			if j - i >= 1:
-				self[i], self[j] = self[j], self[i]
-			j -= 1
+	# 	for i in range(self.size()):
+	# 		if j - i >= 1:
+	# 			self[i], self[j] = self[j], self[i]
+	# 		j -= 1
 
-		return self
+	# 	return self
 
 
--- tests/list_tests.py
+++ tests/list_tests.py
@@ -8,49 +8,49 @@ class MetaListTests(unittest.TestCase):
 		self.list = MetaList()
 		self.list = MetaList(4,5,6)
 
-	def test_is_empty(self):
+	def test_01_is_empty(self):
 		self.assertEqual(self.list.is_empty(), False)
 
-	def test_getter(self):
+	def test_02_getter(self):
 		self.assertEqual(self.list[0], 4)
 
-	def test_setter(self):
+	def test_03_setter(self):
 		self.list[0] = 3
 		self.assertEqual(self.list[0], 3)
 
-	def test_insert_with_one_argument(self):
+	def test_04_insert_with_one_argument(self):
 		# insert automatically inserts an item at the end
 		self.list.insert(1)
 		self.assertEqual(self.list[0], 1)
 
-	def test_insert_with_two_arguments(self):
+	def test_05_insert_with_two_arguments(self):
 		self.list.insert(3, 1)
 		self.assertEqual(self.list[1], 3)
 		self.assertEqual(self.list.storage, [4,3,5,6])
 
-	def test_append(self):
+	def test_06_append(self):
 		self.list.append(7)
 		self.assertEqual(self.list[-1], 7)
 
 		self.assertEqual(self.list.storage, [4,5,6,7])
 
-	def test_extend(self):
+	def test_07_extend(self):
 		self.list.extend([7,8,9])
 		self.assertEqual(self.list.storage, [4,5,6,7,8,9])
 
-	def test_remove(self):
+	def test_08_remove(self):
 		self.list.remove(5)
 		self.assertEqual(self.list.storage, [4,6])
 
-	def test_naked_pop(self):
+	def test_09_naked_pop(self):
 		self.list.pop()
 		self.assertEqual(self.list.storage, [4,5])
 
-	def test_clothed_pop(self):
+	def test_10_clothed_pop(self):
 		self.list.pop(1)
 		self.assertEqual(self.list.storage, [4,6])
 
-	def test_index(self):
+	def test_11_index(self):
 		self.assertEqual(self.list.index(6), 2)
 
 		with self.assertRaises(ItemNotFound):
@@ -59,19 +59,19 @@ class MetaListTests(unittest.TestCase):
 		self.list.append(6)
 		self.assertEqual(self.list.index(6), 2)
 
-	def test_count_with_one_of_each(self):
+	def test_12_count_with_one_of_each(self):
 		self.assertEqual(self.list.count(4), 1)
 
 		self.list.append(4)
 		self.assertEqual(self.list.count(4), 2)
 
-	def test_sort(self):
+	def test_13_sort(self):
 		self.list.append(0)
 		self.list.sort()
 
 		self.assertEqual(self.list.storage, [0,4,5,6])
 
-	def test_reverse(self):
+	def test_14_reverse(self):
 		self.list.reverse()
 		self.assertEqual(self.list.storage, [6,5,4])
 
